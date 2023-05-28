47c044f34e7b994ab3b6d90b411764fa89eb7717
Pearuss <duc12a1cauxe0825@gmail.com>
2023-05-28 21:55:53 +0700

    first commit

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -24,8 +24,44 @@ function Header() {
       </div>
       <div className="header__right">
         <button className="header__btnBuy btn btn--blue">Mua gói</button>
-        <NotificationsOutlinedIcon className="header__bellIcon" />
+        <img src="/bell.svg" alt="" className="header__bellIcon" />
         <img src="./Ellipse20.png" alt="" />
+        <div className="notification">
+          <div className="notification__header">
+            <div className="notification__title">Thông báo</div>
+            <img
+              src="/ThreeDot.svg"
+              alt=""
+              className="notification__view-all"
+            />
+          </div>
+          <div className="notification__list">
+            <div className="notification__item">
+              <div className="notification__viewed"></div>
+              <img
+                src="Avatar.png"
+                alt=""
+                className="notification__user-avatar"
+              />
+              <div className="notification__content">
+                <h4>Tiêu đề video không giới hạn tối đa dòng</h4>
+                <p>3 giờ trước</p>
+              </div>
+              <img
+                src="User.png"
+                alt=""
+                width={80}
+                height={45}
+                className="notification__post"
+              />
+              <img
+                src="ThreeDot.svg"
+                alt=""
+                className="notification__three-dot"
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="header__line"></div>
     </header>

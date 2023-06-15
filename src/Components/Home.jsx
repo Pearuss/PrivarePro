cdeba513aba388fced01961cf01b3c5b4c5c52cd
Pearuss <duc12a1cauxe0825@gmail.com>
2023-06-15 17:30:40 +0700

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import VideoJS from "./VideoJS";
 import videojs from "video.js";
-
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import CommentDialog from "../Dialogs/CommentDialog";
 import ShareDialog from "../Dialogs/ShareDialog";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
 
 function Home() {
   const playerRef = React.useRef(null);
@@ -27,13 +26,6 @@ function Home() {
     ],
   };
 
-  const options = {
-    items: 1,
-    // nav: true,
-    rewind: true,
-    autoplay: true,
-  };
-
   const handlePlayerReady = (player) => {
     playerRef.current = player;
 
@@ -48,26 +40,16 @@ function Home() {
   };
   return (
     <div className="home">
-      {/* <OwlCarousel options={options}>
-          {[1, 2, 3, 4, 5].map((item) => (
-            <div className="item" key={item}>
+      <Swiper
+        direction="vertical"
+        slidesPerView="1"
+        mousewheel={true}
+        className="mySwiper"
+      >
+        {[1, 2, 3, 4].map((item) => (
+          <SwiperSlide key={item}>
+            <div className="player__item">
               <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-            </div>
-          ))}
-        </OwlCarousel> */}
-      <div className="player">
-        <OwlCarousel
-          className="owl-theme"
-          loop
-          margin={10}
-          items={1}
-          nav={true}
-          dots={false}
-        >
-          {[1, 2, 3, 4, 5].map((item) => (
-            <div className="player__item" key={item}>
-              <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-
               <div className="home__feature">
                 <div className="home__feature__group">
                   <button className="home__feature__btn">
@@ -113,9 +95,10 @@ function Home() {
                 </div>
               </div>
             </div>
-          ))}
-        </OwlCarousel>
-      </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
       <CommentDialog open={showComment} setOpen={setShowComment} />
       <ShareDialog open={showShare} setOpen={setShowShare} />
     </div>
